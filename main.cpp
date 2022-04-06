cd5602545ce0b9ab6315c464ccc909bb3d2ad453
Tata_kn18 <karmikday@gmail.com>
2022-04-06 20:49:40 +0300

    update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-
+	double Y = 1;
+	double n = 0;
+	cout << "Vvedite n->";
+	cin >> n;
+	for (size_t i = 0; i < n; i++)
+	{
+		if (floor(n) == n && n < 10)
+		{
+			Y += (1) / (pow(i, 5));
+		}
+	}
+	for (size_t i = 0; i < 10; i++)
+	{
+		Y *= (2 + 1 / );
+	}
 
 }
