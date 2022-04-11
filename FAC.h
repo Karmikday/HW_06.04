9522e9aad5a46560098667a0666e559637499ddf
Tata_kn18 <karmikday@gmail.com>
2022-04-11 15:45:42 +0300

    DONE

---
+++ FAC.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int i)
+{
+	if (i == 0) return 1;
+	else return i * factorial(i - 1);
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include "FAC.h"
 using namespace std;
 int main()
 {
-
-
+	double F;
+	double n = 0, x = 0, b = 0;
+	cout << "Vvedite n->";
+	cin >> n;
+	cout << "Vvedite x->";
+	cin >> x;
+	cout << "Vvedite b->";
+	cin >> b;
+	if (x > 4 && n > 0)
+	{
+		cout << "Way 1" << endl;
+		F = 1;
+		for (size_t k = 1; k <= n; k++)
+		{
+			F *= (1 + (k * k * x / factorial(k)));
+		}
+	}
+	else if (x > 0 && x < 4 && b > 0)
+	{
+		cout << "Way 2" << endl;
+		F = x * sqrt(pow(b, 3));
+	}
+	else
+	{
+		cout << "ERROR";
+	}
+	cout << "Answer" << F << endl;
 }
